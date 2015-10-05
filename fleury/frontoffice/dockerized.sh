fa9269208ff61a8094a916a7ef6c0da71e05e0a8
Marcelo M. Fleury <marcelomf@gmail.com>
2015-10-05 16:27:04 -0300

    The end...

--- fleury/frontoffice/dockerized.sh
+++ fleury/frontoffice/dockerized.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+cd /opt/challenge/middleware
+node index.js &
 cd /opt/challenge/frontoffice
+/bin/bash -c "/usr/bin/mongod --smallfiles &"
+sleep 3
+grao main:create:admin --username marcelo --name Marcelo --email marcelomf@gmail.com --password admin123
+sleep 3
+#service mongodb start
 node index.js
