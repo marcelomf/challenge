2cf6f53b636af9aacaf5154e41712554cd036893
Marcelo M. Fleury <marcelomf@gmail.com>
2015-10-05 16:33:39 -0300

    add sudo

--- fleury/rundocker.sh
+++ fleury/rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker build -t challenge .
-docker run -p 8015:8015 -p 8016:8016 -p 8080:8080 -ti --rm -v /home/marcelo/Projetos/challenge/fleury:/opt/challenge challenge
+#sudo docker build -t challenge .
+sudo docker run -p 8015:8015 -p 8016:8016 -p 8080:8080 -ti --rm -v /home/marcelo/Projetos/challenge/fleury:/opt/challenge challenge
